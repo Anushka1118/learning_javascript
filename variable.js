3fbf8a272dfa616e83eab442c5cca3c17a58dfe8
Anushka1118 <anushka.k.1118@gmail.com>
2025-09-28 04:03:03 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+const accountId = "143324"
+let accountEmai = "abc@gmail.com"
+var password = 12345
+city = "jaipur"
+//accountId = 2  const are immutable
+accountEmai = "abvss@goooglr.in"
+password = 1111
+let sum;
+/*
+smicolon are mostly optional.
+prefer not to use var because of issue in block scope and functionaal scope.
+undeclared variable are undefined.
+*/
+
+console.log(accountId);
+console.table([accountEmai, accountId, password, city, sum])
